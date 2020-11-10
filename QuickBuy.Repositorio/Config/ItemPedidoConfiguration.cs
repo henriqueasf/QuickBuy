847a40d58cdbcd5a2b9f60184ab8349a5cfeab78
Henrique ASF <henrique@serel.com.br>
2020-11-10 19:21:08 -0300

    até aula 36

---
+++ QuickBuy.Repositorio/Config/ItemPedidoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class ItemPedidoConfiguration : IEntityTypeConfiguration<ItemPedido>
+    {
+        public void Configure(EntityTypeBuilder<ItemPedido> builder)
+        {
+            builder.Property(i => i.Id);
+
+            builder
+                .Property(i => i.ProdutoId)
+                .IsRequired();
+
+            builder
+                .Property(i => i.Quantidade)
+                .IsRequired();
+
+
+
+        }
+    }
+}
---
+++ QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            // builder utiliza o padrão fluent
+            builder
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(u => u.Senha)
+                .IsRequired()
+                .HasMaxLength(400);
+
+            builder
+                .Property(u => u.Nome)
+                .IsRequired()
+                .HasMaxLength(45);
+                //.HasColumnType("varchar");
+
+            builder
+                .Property(u => u.SobreNome)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            //builder.Property(u => u.Pedidos);
+
+        }
+    }
+}
